b6c4613fd3ff996a0e32c449a0597c3cd1023790
lolluslollus <lolluslollus@users.noreply.github.com>
2022-02-11 02:11:10 +0100

    prepare release

--- strings.lua
+++ strings.lua
@@ -3,64 +3,19 @@ function data()
 		en = {
             ["ModDesc"] =
                 [[
-[h1]EARLY BETA VERSION - EXPECT BUGS AND INCOMPATIBILITIES[/h1]
-At this time I make no promises about feature-completeness or stability.
-Depending on feedback and bugs I may have to rework things that could cause this mod to stop working on earlier save games.
+                    Displays for incoming and outgoing vehicles.
+                    Turn the updates on or off with the bottom bar.
 
-[b]DURING BETA, PLEASE BACK UP YOUR SAVE GAMES BEFORE SAVING THIS MOD IN THEM[/b]
-Pretty good general advice when experimenting with new mods, really :)
+                    I have been thinking long and hard whether to publish this mod or not, since I copied the idea from badgerrhax, However, this is my own code and my own models, and this is certainly not paid work, so I do.
 
-I'm making this available for people to help with testing if they wish.
-
-[b]What I'd like help with[/b]
-- Feedback on how well it performs on various computers and map sizes, station sizes, etc.
-- Feedback about the functionality, what is good, bad, missing
-- Mods that might stop this working - e.g. the Timetables mod which I am already investigating for compatibility
-
-[b]Logging is enabled for the beta period[/b]
-- Update timing
-- Selected sign details
-- Selected vehicle time to arrival at each station with a sign on
-
-If you report performance or timing issues I may request that you provide this info from your stdout.txt.
-
-When I am happy with the quality and performance I will remove all these beta warnings.
-
-[h1]Main Features[/h1]
-* = refer to known issues and limitations for clarifications
-- [b]Single Terminal Arrivals Display[/b] - place it on a platform and it will automatically* display the next arriving trains to that platform
-- [b]Station Departures Display[/b] - place within 50m* of a station and it will display up to the next 8 trains and their destinations / platform / departure times
-
-[h1]Planned Features[/h1]
-I'm planning on extending the mod to support signs displaying the following type of information
-- Single Terminal for one vehicle with list of "calling at" stations
-- Station Arrivals Display (showing origins instead of destinations)
-
-[h1]Known issues[/h1]
-These are things I've identified as needing more work
-- Must be placed within 50m of a station - this distance is abitrary and open to feedback on reasonable values
-- The terminal detection needs improvement - if you place it too far from where the train stops it'll likely get it wrong. There's a terminal override on the asset parameters for now.
-- Line destination calculations may be wrong for some lines - it depends how they are defined. If you have lines that it gets wrong, please provide the list of stops and expected destinations. It may or may not be possible to automatically calculate - e.g. I don't think it'll ever work for "circular" lines without manual configuration
-- Detection of nearby street bus stations is only semi-functional - especially when there is a terminal on both sides of the road. Work in progress.
-- General code optimisations will be done once the functionality is solid, to speed up the station updates
-
-[h1]Limitations[/h1]
-These are things I don't believe can be much better than they are right now
-- The ETA calculations are based on previous arrival times and segment travel times - if the vehicle has not travelled the line at least once, this data will be inaccurate but will improve over time.
-- [b]You must pause the game before editing / deleting the assets[/b] - the asset is regularly "replaced" so by the time you've clicked bulldoze, the thing you tried to bulldoze isn't there anymore.
-
-[h1]Extensibility[/h1]
-This is designed to work as a base mod for other modders to create their own displays too. There's a construction registration API where you can tell it about your
-display construction and it will manage its display updates when placed in game. See the comments in mod.lua and how the included constructions use the data the engine provides.
-
-[b]Please report any bugs with this mod so I can try to address them.[/b]
+                    These things can be performance-intensive, so you can switch them on and off from the bottom bar (read above).
 			]],
             ["ModName"] = "Dynamic Departures / Arrivals Displays",
             ["AlignFree"] = 'Free',
             ["AlignPlatform"] = 'Platform',
-            ["displays"] = "displays",
             ["ArrivalsAllCaps"] = "ARRIVALS",
             ["Auto"] = "Auto",
+            ["displays"] = "displays",
             ["DynamicDisplaysOff"] = "Dynamic Displays OFF",
             ["DynamicDisplaysOn"] = "Dynamic Displays ON",
             ["Cargo"] = "Cargo",
