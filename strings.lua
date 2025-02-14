9ff5e21f2c5075973af60056b36d2a05d4774105
lolluslollus <lolluslollus@users.noreply.github.com>
2025-02-14 10:17:38 +0100

    Update strings.lua

--- strings.lua
+++ strings.lua
@@ -134,21 +134,21 @@ Another big thank you goes out to badgerrhax for the idea.
             ["ModDesc"] =
                 [[
 Tabelloni per veicoli in arrivo e in partenza, si trovano nel menu \"assets\".
-Turn the updates on or off with the bottom bar. When turned on, they update every 5 seconds.
-If you add a pair of brackets to your line names, the displays will show the stuff between the brackets, same as https://steamcommunity.com/sharedfiles/filedetails/?id=2528202101. The only difference is: if you add no brackets, the whole line name will be displayed. You can play with this, here some examples: "(IC 335) London - Rosenheim" will show as "IC 335". "Schnellzug (Flieg)ende( Kartof)fel" will show as "Flieg Kartof". Anything plus () will show no name at all.
+Attiva o disattiva gli aggiornamenti con la barra in basso. Quando sono accesi, si aggiornano ogni 5 secondi.
+Se aggiungi un paio di parentesi ai nomi delle linee, i tabelloni mostreranno i caratteri fra le parentesi, come https://steamcommunity.com/sharedfiles/filedetails/?id=2528202101. L'unica differenza è: se non si aggiungono parentesi, verrà visualizzato l'intero nome della riga. Puoi sperimentare, ecco alcuni esempi: "(IC 335) London - Rosenheim" verrà visualizzato come "IC 335". "(Rap)ido Andrea (Doria)" apparirà come "Rap Doria". Qualunque cosa più () non mostrerà alcun nome.
 
-I have been thinking long and hard whether to publish this mod or not, since I copied the idea from badgerrhax. However, this is my own code and my own models, so I do. If you don't like it, don't use it.
+Ho pensato a lungo se pubblicare o meno questa mod, dato che ho copiato l'idea da badgerrhax. Alla fine questo è il mio codice e i miei modelli, quindi l'ho fatto. Se non ti piace, non usarla.
 
-NOTES:
-1) These things can be performance-intensive, so you can switch them on and off from the bottom bar (toggle the orange icon). Some other mods, such as advanced statistics, priority signals or shunting, hog game_script.update(). The game can only take so much, so you might need to choose.
-2) If you think they are not working, check your bottom bar (toggle the orange icon), unpause the game and wait a little.
-3) Once you attached a display to a station, it will be tied to it forever. If you bulldoze the station, its displays will disappear automatically, as soon as you unpause the game and the station dies.
-4) Accuracy improves over time.
-5) Accuracy goes down when you add or remove vehicles, it picks up again after a bit.
-6) Rearrange these at any time with https://steamcommunity.com/sharedfiles/filedetails/?id=2748222965.
+NOTE:
+1) Questi tabelloni possono succhiare molta CPU, quindi puoi attivarli e disattivarli dalla barra in basso (usa l'icona arancione). Esistono altre mod, come advanced statistics, priority signals or shunting, che adoperano game_script.update(). Il gioco pone dei limiti, quindi a volte bisogna scegliere.
+2) Se pensi che non funzionino, controlla la barra in basso (usa l'icona arancione) e aspetta un po'.
+3) Una volta collegato un tabellone a una stazione, sarà legato ad essa per sempre. Se demolisci la stazione i suoi tabelloni scompariranno automaticamente, non appena riprendi il gioco e la stazione muore.
+4) La precisione migliora nel tempo.
+5) La precisione diminuisce quando aggiungi o rimuovi veicoli e torna migliore dopo un po'.
+6) Sposta questi tabelloni quando vuoi con https://steamcommunity.com/sharedfiles/filedetails/?id=2748222965.
 
-A big thank you goes out to Mr F from UG, this mod would have never worked without his help.
-Another big thank you goes out to badgerrhax for the idea.
+Un grande ringraziamento va a Mr F di Urban Games; questa mod non avrebbe mai funzionato senza il suo aiuto.
+Un altro grande ringraziamento va a Badgerrhax per l'idea.
 			]],
             ["ModName"] = "Tabelloni dinamici per arrivi e partenze",
             ["Align2Platform"] = "Allinea alla pensilina",
@@ -163,13 +163,13 @@ Another big thank you goes out to badgerrhax for the idea.
             ["DeparturesAllCaps"] = "PARTENZE",
             ["Due"] = "!!!",
             ["PlatformDeparturesDisplayName"] = "Tabellone dinamico per binari ferroviari",
-            ["PlatformDeparturesDisplayDesc"] = "A digital display showing the next two trains approaching a terminal. It will be bulldozed if the station is bulldozed and you unpause the game.",
+            ["PlatformDeparturesDisplayDesc"] = "Un tabellone digitale che mostra i prossimi due treni in arrivo al binario. Si autodistruggerà se distruggi la stazione e riprendi il gioco.",
             ["StationArrivalsDisplayName"] = "Tabellone dinamico per arrivi",
-            ["StationArrivalsDisplayDesc"] = "A digital display showing vehicles approaching all terminals at a nearby station. It will be bulldozed if the station is bulldozed and you unpause the game.",
+            ["StationArrivalsDisplayDesc"] = "Un tabellone digitale che mostra i veicoli in arrivo a tutti i binari/marciapiedi/altro di una stazione/aeroporto/porto nelle vicinanze. Si autodistruggerà se distruggi la stazione e riprendi il gioco.",
             ["StationDeparturesDisplayName"] = "Tabellone dinamico per partenze",
-            ["StationDeparturesDisplayDesc"] = "A digital display showing vehicles departing from all terminals at a nearby station. It will be bulldozed if the station is bulldozed and you unpause the game.",
+            ["StationDeparturesDisplayDesc"] = "Un tabellone digitale che mostra i veicoli in partenza da tutti i binari di una stazione/aeroporto/porto nelle vicinanze. Si autodistruggerà se distruggi la stazione e riprendi il gioco.",
             ["StreetPlatformDeparturesDisplayName"] = "Tabellone dinamico per fermate",
-            ["StreetPlatformDeparturesDisplayDesc"] = "A digital display showing the next two vehicles approaching a terminal. It will be bulldozed if the station is bulldozed and you unpause the game.",
+            ["StreetPlatformDeparturesDisplayDesc"] = "Un tabellone digitale che mostra i prossimi due veicoli in arrivo al marciapiede. Si autodistruggerà se distruggi la stazione e riprendi il gioco.",
             ["CannotFindStationToJoin"] = "Non vedo stazioni qui vicino",
             ["From"] = "Origine",
             ["GoBack"] = "Indietro",
